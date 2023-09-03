2c08da34002ee5899a9e05617f58cce0028c8015
yingsw <805962304@qq.com>
2023-09-04 00:17:16 +0800

    update

--- Algorithms/基础/哈希/哈希表/判断集合相等或包含/相等/LC1460.cpp
+++ Algorithms/基础/哈希/哈希表/判断集合相等或包含/相等/LC1460.cpp
@@ -7,7 +7,7 @@ typedef unsigned long long ULL;
 typedef pair<int, int> PII;
 const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
 
-// https://leetcode.cn/problems/make-two-arrays-equal-by-reversing-sub-arrays/
+// https://leetcode.cn/problems/make-two-arrays-equal-by-reversing-subarrays/
 
 class Solution {
 public:
@@ -22,27 +22,27 @@ public:
     }
 };
 
-class Solution {
-public:
-    bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        unordered_map<int, int> mp;
-        int diff = 0, n = target.size();
-        for (int i = 0; i < n; i++) {
-            int x = target[i], y = arr[i];
-            if (mp[x] == 0)
-                ++diff;
-            else if (mp[x] == -1)
-                --diff;
-            ++mp[x];
-            if (mp[y] == 1)
-                --diff;
-            else if (mp[y] == 0)
-                ++diff;
-            --mp[y];
-        }
-        return diff == 0;
-    }
-};
+// class Solution {
+// public:
+//     bool canBeEqual(vector<int>& target, vector<int>& arr) {
+//         unordered_map<int, int> mp;
+//         int diff = 0, n = target.size();
+//         for (int i = 0; i < n; i++) {
+//             int x = target[i], y = arr[i];
+//             if (mp[x] == 0)
+//                 ++diff;
+//             else if (mp[x] == -1)
+//                 --diff;
+//             ++mp[x];
+//             if (mp[y] == 1)
+//                 --diff;
+//             else if (mp[y] == 0)
+//                 ++diff;
+//             --mp[y];
+//         }
+//         return diff == 0;
+//     }
+// };
 
 int main() {
     return 0;
