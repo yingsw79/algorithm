7a08edffdc191a1e49cf0e2d6af4e7c9f10f4247
yingsw <805962304@qq.com>
2023-10-24 20:27:34 +0800

    2023-10-24

--- Algorithms/动态规划/背包/分组背包/LC1155.cpp
+++ Algorithms/动态规划/背包/分组背包/LC1155.cpp
@@ -8,6 +8,7 @@ const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
 
 // https://leetcode.cn/problems/number-of-dice-rolls-with-target-sum/
 
+// 1.
 class Solution {
 public:
     int numRollsToTarget(int n, int m, int t) {
@@ -23,6 +24,7 @@ public:
     }
 };
 
+// 2.
 class Solution {
 public:
     int numRollsToTarget(int n, int k, int target) {
@@ -32,13 +34,51 @@ public:
         f[0] = 1;
         for (int i = 1; i <= n; i++) {
             for (int j = target; j >= 0; j--) {
-                f[j] = 0;  //不能从f[i-1][j]转移,必须要选
+                f[j] = 0;  // 不能从f[i-1][j]转移, 必须要选!!!!!!!!!!!!!!!!!!!!!!!
                 for (int u = 1; u <= k && u <= j; u++) {
                     f[j] = (f[j] + f[j - u]) % MOD;
                 }
             }
         }
         return f[target];
+    }
+};
+
+class Solution {
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        const int MOD = 1e9 + 7;
+        int f[target - n + 1];  // 0 ~ k-1
+        memset(f, 0, sizeof f);
+        f[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = target - n; j >= 0; j--) {
+                for (int u = 1; u < k && u <= j; u++) {  // 可以从f[i-1][j]转移, 因为0相当于不选
+                    f[j] = (f[j] + f[j - u]) % MOD;
+                }
+            }
+        }
+        return f[target - n];
+    }
+};
+
+// 3.前缀和优化
+class Solution {
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        const int MOD = 1e9 + 7;
+        int f[target - n + 1];  // 0 ~ k-1
+        memset(f, 0, sizeof f);
+        f[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target - n; j++) {
+                f[j] = (f[j] + f[j - 1]) % MOD;
+            }
+            for (int j = target - n; j >= k; j--) {
+                f[j] = (f[j] - f[j - k] + MOD) % MOD;
+            }
+        }
+        return f[target - n];
     }
 };
 
--- Algorithms/动态规划/背包/分组背包/多重背包/LC2902.go
+++ Algorithms/动态规划/背包/分组背包/多重背包/LC2902.go
@@ -1,10 +1,12 @@
 package main
 
 // https://leetcode.cn/problems/count-of-sub-multisets-with-bounded-sum/description/
+// https://leetcode.cn/problems/number-of-dice-rolls-with-target-sum/
+// 前缀和优化
 
 func countSubMultisets(nums []int, l int, r int) (res int) {
 	const MOD int = 1e9 + 7
-	f, g := make([]int, r+1), make([]int, r+1)
+	f := make([]int, r+1)
 	f[0] = 1
 	mp := map[int]int{}
 	for _, v := range nums {
@@ -14,20 +16,11 @@ func countSubMultisets(nums []int, l int, r int) (res int) {
 		if k == 0 {
 			continue
 		}
-		for i := range g {
-			g[i] = 0
+		for i := k; i <= r; i++ {
+			f[i] = (f[i] + f[i-k]) % MOD
 		}
-		for i := range g {
-			g[i] = f[i]
-			if i >= k {
-				g[i] = (g[i] + g[i-k]) % MOD
-			}
-		}
-		for i := range g {
-			f[i] = g[i]
-			if i >= (v+1)*k {
-				f[i] = (f[i] - g[i-(v+1)*k] + MOD) % MOD
-			}
+		for i, t := r, (v+1)*k; i >= t; i-- {
+			f[i] = (f[i] - f[i-t] + MOD) % MOD
 		}
 	}
 	for i := l; i <= r; i++ {
