ca1d1e62d9dd2ad036016e7410a4d776f03b5346
ysw <805962304@qq.com>
2024-05-22 00:20:34 +0800

    update

---
+++ 数据结构/平衡树/前k大/LC3013.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define lowbit(x) (x & (-x))
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int, int> PII;
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+
+// https://leetcode.cn/problems/divide-an-array-into-subarrays-with-minimum-cost-ii/description/
+
+// class Solution {
+// public:
+//     using LL = long long;
+//     long long minimumCost(vector<int>& nums, int k, int dist) {
+//         k--;
+//         multiset<int> l, r;
+//         LL res = 0, sum = nums[0];
+//         for (int i = 1; i < nums.size(); i++) {
+//             if (l.empty() || nums[i] < *l.rbegin()) {
+//                 l.insert(nums)
+//             }
+//         }
+//     }
+// };
+
+int main() {
+    return 0;
+}
